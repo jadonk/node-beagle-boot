9b645515a6af437705e218f7372b55c956a7273b
Jason Kridner <jdk@ti.com>
2018-03-06 18:32:00 -0500

    test.js: update paths

--- test.js
+++ test.js
@@ -5,9 +5,9 @@ var umsMountPoint = '/media';
 var umsVolume = '/BOOT';
 
 var emitter = BB.tftpServer([
-    {vid: 0x0451, pid: 0x6141, file_path: './bin/spl'}, // ROM
-    {vid: 0x0451, pid: 0xd022, file_path: './bin/uboot'}, // SPL
-    {vid: 0x0525, pid: 0xa4a2, file_path: './bin/uboot'}, // SPL
+    {vid: 0x0451, pid: 0x6141, file_path: './bin/u-boot-spl.bin'}, // ROM
+    {vid: 0x0451, pid: 0xd022, file_path: './bin/u-boot.img'}, // SPL
+    {vid: 0x0525, pid: 0xa4a2, file_path: './bin/u-boot.img'}, // SPL
 ]);
 
 console.log('USB tester and EEPROM writer started');
